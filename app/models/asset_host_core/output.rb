bc9b495a0a3c695b472648e707c709009cc83141
Eric Richardson <erichardson@kpcc.org>
2012-02-22 16:01:19 -0800

    Pull README from master; Fix AssetOutput invalidation from Output change

--- app/models/asset_host_core/output.rb
+++ app/models/asset_host_core/output.rb
@@ -2,8 +2,7 @@ module AssetHostCore
   class Output < ActiveRecord::Base
     has_many :asset_outputs
     
-    after_save :delete_asset_outputs, 
-      :if => Proc.new { |o| o.previous_changes.has_key?("size") || o.previous_changes.has_key?("extension")}
+    after_save :delete_asset_outputs, :if => Proc.new { |o| o.size_changed? || o.extension_changed? }
     
     #----------
     
@@ -32,13 +31,13 @@ module AssetHostCore
     
     protected
     
-    def delete_asset_outputs(o)
+    def delete_asset_outputs
       # for each AssetOutput, we need to delete the object and queue the file 
       # for deletion via Paperclip
       
-      o.asset_outputs.each do |ao|
+      self.asset_outputs.each do |ao|
         # first delete the file
-        ao.asset.image.delete_style ao.code_sym
+        ao.asset.image.delete_style ao.output.code_sym
         
         # next delete the AssetOutput
         ao.destroy()
