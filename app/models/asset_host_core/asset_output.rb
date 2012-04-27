e2a9a794fcd068ef6ec407fa5d083db0d8a10082
Eric Richardson <erichardson@kpcc.org>
2012-04-27 10:53:40 -0700

    Fix bug where AssetOutput path cache was getting rewritten on destroy

--- app/models/asset_host_core/asset_output.rb
+++ app/models/asset_host_core/asset_output.rb
@@ -4,7 +4,7 @@ module AssetHostCore
     belongs_to :output
     
     before_save :delete_cache_and_img, :if => Proc.new { |ao| ao.fingerprint_changed? || ao.image_fingerprint_changed? }
-    before_destroy :delete_cache_and_img
+    before_destroy :delete_cache_and_img_and_fingerprint
     
     after_commit :cache_img_path, :if => Proc.new { |ao| ao.image_fingerprint? && ao.fingerprint? }
 
@@ -24,7 +24,8 @@ module AssetHostCore
       if finger && imgfinger
         # -- delete our old cache -- #
         Rails.logger.debug("deleting cache at #{"img:"+[self.asset.id,imgfinger,self.output.code].join(":")}")
-        Rails.cache.delete("img:"+[self.asset.id,imgfinger,self.output.code].join(":"))
+        resp = Rails.cache.delete("img:"+[self.asset.id,imgfinger,self.output.code].join(":"))
+        Rails.logger.debug("cache.delete got #{resp}")
         
         # -- delete our AssetOutput -- #
         path = self.asset.image.path(self)
@@ -43,13 +44,25 @@ module AssetHostCore
     
     #----------
     
+    # on destroy, we need to do the normal save deletes and also delete our fingerprint
+    def delete_cache_and_img_and_fingerprint
+      self.delete_cache_and_img()
+      
+      # why do we bother clearing our fingerprint if the AssetOutput itself 
+      # is about to get deleted? If we don't, the after_commit handler will 
+      # rewrite the same cache we just deleted.
+      self.fingerprint = ''
+    end
+    
+    #----------
+    
     def cache_img_path
       # -- in with the new -- #
       path = self.asset.image.path(self)
       
       Rails.logger.debug("AssetOutput cache_img_path for #{self.asset.id}/#{self.output.code_sym} got #{path}")
       
-      if path
+      if path && File.exists?(path)
         Rails.cache.write("img:"+[self.asset.id,self.image_fingerprint,self.output.code].join(":"),path)
       end
       
